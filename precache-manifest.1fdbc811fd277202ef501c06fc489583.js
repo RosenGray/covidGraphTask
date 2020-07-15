47ded8d813b238cfcc4c5fc023ffd68f96ee2c46
Vladi Iokhim <vladi.iokhim@tamooz.eu>
2020-07-15 16:21:39 +0300

    Updates

---
+++ precache-manifest.1fdbc811fd277202ef501c06fc489583.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c952d4d80e0c82fdd33d21e659c03d30",
+    "url": "/%7BcovidGraphTask%7D/index.html"
+  },
+  {
+    "revision": "a92bea8abd0d6efcb210",
+    "url": "/%7BcovidGraphTask%7D/static/css/main.dfc26e5d.chunk.css"
+  },
+  {
+    "revision": "4b722b92f66824d07027",
+    "url": "/%7BcovidGraphTask%7D/static/js/2.ed650832.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/%7BcovidGraphTask%7D/static/js/2.ed650832.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a92bea8abd0d6efcb210",
+    "url": "/%7BcovidGraphTask%7D/static/js/main.15748409.chunk.js"
+  },
+  {
+    "revision": "21be3e23bc5436153ec4",
+    "url": "/%7BcovidGraphTask%7D/static/js/runtime-main.61e9ef47.js"
+  }
+]);
